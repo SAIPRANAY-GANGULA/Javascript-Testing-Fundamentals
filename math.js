6a618f6da5298b02dc2c4bb0e03088195f9ad982
saig <saig@backbase.com>
2022-07-04 12:52:58 +0530

    create new assertion method without using assertion library

---
+++ math.js
@@ -0,0 +1,7 @@
+function add(a, b) {
+  return a + b;
+}
+
+const subtract = (a, b) => a - b;
+
+module.exports = { subtract, add };
--- sample-test.js
+++ sample-test.js
@@ -1,19 +1,29 @@
-function add(a, b) {
-    return a + b;
-}
-
-const subtract = (a, b) => a - b;
+const { add, subtract } = require("./math");
 
 let result = add(5, 5);
 let expected = 10;
 
-if (result !== expected) {
-    throw new Error(`${result} is not equal to ${expected}`)
-}
+// if (result !== expected) {
+//   throw new Error(`${result} is not equal to ${expected}`);
+// }
+expect(result).toBe(expected);
 
 result = subtract(5, 4);
 expected = 1;
 
-if (result !== expected) {
-    throw new Error(`${result} is not equal to ${expected}`)
+// if (result !== expected) {
+//   throw new Error(`${result} is not equal to ${expected}`);
+// }
+expect(result).toBe(expected);
+
+function expect(result) {
+  return {
+    toBe(expected) {
+      if (result !== expected) {
+        throw new Error(`${result} is not equal to ${expected}`);
+      }
+    },
+    equals(expected) {},
+    graterThan(expected) {},
+  };
 }
